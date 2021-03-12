32a98ea800ce75cc2e92f7539721d9e7003394c2
Your Name <you@example.com>
2021-03-12 17:44:57 -0500

    add: create structure monorepo-pastry

---
+++ libs/pastry-commons/src/lib/pastry-commons.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PastryCommonsComponent } from './pastry-commons.component';
+
+
+
+@NgModule({
+  declarations: [PastryCommonsComponent],
+  imports: [
+  ],
+  exports: [PastryCommonsComponent]
+})
+export class PastryCommonsModule { }
